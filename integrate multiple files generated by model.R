0b56e1463b0cfdb1de6400a01015c3efff09366b
Kaining26 <kainingzhou26@gmail.com>
2022-06-15 17:38:59 +0300

    Create the first script

---
+++ integrate multiple files generated by model.R
@@ -0,0 +1,4 @@
+#===Task======
+# Adam's model could not automatically batch processing multiple image folders.
+# Need to work folder by folder, therefore there will be multiple .csv files from different dates.
+# This script is to fuse multiple files into one datasheet. It will be easier for later use.
